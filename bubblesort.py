e419fd6705cdb8dfbb4b6307ddb6c55c4a208281
Mryadavg999 <101015492+Mryadavg999@users.noreply.github.com>
2023-10-08 14:58:17 +0530

    Create bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,26 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    # Traverse through all array elements
+    for i in range(n):
+        # Flag to optimize the algorithm
+        swapped = False
+
+        # Last i elements are already in place, so we don't need to check them
+        for j in range(0, n - i - 1):
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        # If no two elements were swapped in the inner loop, the array is already sorted
+        if not swapped:
+            break
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+
+print("Sorted array is:")
+for element in arr:
+    print(element, end=" ")
